78b500ba0c8897c39eb8c4b46780032c670a76b3
rickynyairo <rickynyairo@gmail.com>
2019-07-17 18:22:43 +0300

    removed unneccesary build from config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,13 +9,11 @@ module.exports = {
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
-    env: {
+    env_development: {
       NODE_ENV: 'development',
-      PORT: 3001
     },
     env_production: {
       NODE_ENV: 'production',
-      PORT: 3000
     }
   }],
 
